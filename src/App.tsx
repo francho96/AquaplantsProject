4f279f2aadf67563874279600685150277eac6d7
francho96 <franco.barria@usach.cl>
2024-05-29 19:44:44 -0400

    HABEMUS CONEXION LA CTM QUE RICO OOHH ME VENGOOO

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -14,27 +14,30 @@ const Login = lazy(() => import('./routes/login/Login'));
 const Root = lazy(() => import('./routes/root'));
 const Home = lazy(() => import('./routes/home/Home'));
 
+export const UserContext = createContext(null);
 
 function App() {
     return (
         <BrowserRouter>
-            <Suspense fallback={<div></div>}>
-                <Routes>
-                    <Route
-                        index
-                        element={<Login />}
-                    />
-                    <Route path="/in" element={<Root />}>
-                        <Route index element={<Home />} />
-                        <Route path='shopping' element={<Shopping />} />
-                        <Route path='user' element={<User />} />
-                        <Route path='refill' element={<Refill />} />
-                        <Route path='refill/fertilizante' element={<Fertilizante />} />
-                        <Route path='refill/almacigo' element={<Almacigo />} />
-                        <Route path='mail' element={<Mail />} />
-                    </Route>
-                </Routes>
-            </Suspense>
+            <UserContext.Provider value={null}>
+                <Suspense fallback={<div></div>}>
+                    <Routes>
+                        <Route
+                            index
+                            element={<Login />}
+                        />
+                        <Route path="/in" element={<Root />}>
+                            <Route index element={<Home />} />
+                            <Route path='shopping' element={<Shopping />} />
+                            <Route path='user' element={<User />} />
+                            <Route path='refill' element={<Refill />} />
+                            <Route path='refill/fertilizante' element={<Fertilizante />} />
+                            <Route path='refill/almacigo' element={<Almacigo />} />
+                            <Route path='mail' element={<Mail />} />
+                        </Route>
+                    </Routes>
+                </Suspense>
+            </UserContext.Provider>
         </BrowserRouter>
     );
 }
--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,70 +1,124 @@
-import "./styles.css";
-import AquaplantsLogo from "../../assets/Logo_AquaPlants_letraverde_fondotransparente.png"
-import { useNavigate } from "react-router-dom";
-import Card from "../components/Card";
-import { useEffect, useState } from "react";
+import './styles.css';
+import AquaplantsLogo from '../../assets/Logo_AquaPlants_letraverde_fondotransparente.png';
+import { useNavigate } from 'react-router-dom';
+import Card from '../components/Card';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export default function App() {
+    const [entering, setEntering] = useState(false);
+    const [showCover, setShowCover] = useState(true);
 
-  const [entering, setEntering] = useState(false)
-  const [showCover, setShowCover] = useState(true)
-  const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowCover(false);
-    }, 1000);
-  
-    return () => clearTimeout(timer);
-  }, []);
-  
-  const handleEnter = () => {
-    setEntering(!entering);
-    setTimeout(() => {
-      navigate("/in");
-    }, 3000);
-  };
+    const [error, setError] = useState(false);
+    const navigate = useNavigate();
 
-  return (
-    <div className="App">
-      {showCover && (
-          <div className="green-screen"></div>
-        )}
-      <div className="wrapper">
-        
-        <div className={entering ? "dummyBackgroundEntering" : "dummyBackground"}>
-          
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowCover(false);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    const handleEnter = async () => {
+        try {
+          const url = import.meta.env.VITE_APP_XDD;
+          console.log(url)
+          const { data } = await axios.post(url + "/login", {email, password});
+          console.log(data);
+          setEntering(!entering);
+          setTimeout(() => {
+              navigate('/in');
+          }, 3000);
+        } catch (error) {
+          console.log("no funciono");
+          setError(true);
+        }
+    };
+
+    return (
+        <div className="App">
+            {showCover && <div className="green-screen"></div>}
+            <div className="wrapper">
+                <div
+                    className={
+                        entering ? 'dummyBackgroundEntering' : 'dummyBackground'
+                    }
+                ></div>
+                <img
+                    className="heading"
+                    src={AquaplantsLogo}
+                    width="150px"
+                    style={{ marginTop: '50px', marginBottom: '50px' }}
+                ></img>
+                <div className={entering ? 'disappear' : 'appear'}>
+                    <Card>
+                        <div style={{ padding: '10px' }}>
+                            <input
+                                className="input"
+                                id="email"
+                                disabled={entering}
+                                placeholder="Email"
+                                type="email"
+                                style={{ marginBottom: '10px' }}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+
+                            <input
+                                className="input"
+                                id="password"
+                                disabled={entering}
+                                placeholder="Contraseña"
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                           
+                                <div
+                                    style={{
+                                        color: 'red',
+                                        textAlign: 'left',
+                                        marginTop: '10px',
+                                    }}
+                                >
+                                     {error && "Usuario o contraseña incorrectos"} &nbsp;
+                                </div>
+                            <div
+                                className="space"
+                                style={{ justifyContent: 'end' }}
+                            >
+                                <a className="changePassword">
+                                    ¿Olvidaste la contraseña?
+                                </a>
+                            </div>
+                            <div className="lower-form">
+                                <button
+                                    disabled={entering}
+                                    className="button"
+                                    style={{ textAlign: 'center' }}
+                                    onClick={handleEnter}
+                                >
+                                    Entrar
+                                </button>
+                                <span>
+                                    <a className="register" href="https://aquaplants.cl/web/">
+                                        ¿Aún no eres Aquaplanter?
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+                <div
+                    className={
+                        entering
+                            ? 'dummyBackground2Entering'
+                            : 'dummyBackground2'
+                    }
+                ></div>
+            </div>
         </div>
-        <img className="heading" src={AquaplantsLogo} width="150px" style={{marginTop: "50px", marginBottom: "50px"}}></img>
-          <div className={entering ? "disappear" : "appear"}>
-            <Card>
-              <div style={{padding: "10px"}}>
-              <input className="input" id="email" disabled={entering} placeholder="Email" type="email" style={{marginBottom: "10px"}} />
-
-              <input
-                className="input"
-                id="password"
-                disabled={entering}
-                placeholder="Contraseña"
-                type="password"
-              />
-              <div className="space" style={{justifyContent: "end"}}>
-                <a className="changePassword">¿Olvidaste la contraseña?</a>
-              </div>
-              <div className="lower-form">
-              <button  disabled={entering} className="button" style={{textAlign: "center"}} onClick={handleEnter}>
-                Entrar
-              </button>
-                <span>
-                  <a className="register" href="#">
-                    ¿Aún no eres Aquaplanter?
-                  </a>
-                </span>
-              </div>
-              </div>
-          </Card>
-        </div>
-        <div className={entering ? "dummyBackground2Entering" : "dummyBackground2"}></div>
-      </div>
-    </div>
-  );
+    );
 }
